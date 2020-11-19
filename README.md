2d292c8cfbe102dfe1a7f2b2879efa51e107b124
unknown <engahmedgomaa97@gmail.com>
2020-11-19 15:44:49 +0200

    handling the navigation creating with JS.

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
-# Landing-page
+-------------------
+What's this app?
+-------------------
+
+This is portfolio app contain my info and my projects collections.
+
+-------------------
+The Contents of the app.
+-------------------
+It contains 4 parts:
+1. About section.
+2. skills section.
+3. projects section.
+4. contacts section.
+
+-------------------
+The files structure.
+-------------------
+
+1. css file (style.css).
+2. js file (app.js).
+3. html file (index.html).
--- js/app.js
+++ js/app.js
@@ -1,6 +1,60 @@
+//****************************
+// Building the nav
+//****************************
+const navbar = document.querySelector('.navbar__menu')
+const myDocFrag = document.createDocumentFragment();
+
+// creating left nav
+const leftList = document.createElement('ul');
+leftList.setAttribute("class", "navbar__left");
+leftList.innerHTML= '<li><a>Ahmed Gomaa</a></li>';
+
+myDocFrag.appendChild(leftList);
+
+// creating right nav
+const rightList = document.createElement('ul');
+rightList.setAttribute('class', 'navbar__right');
+
+const aboutItem = document.createElement('li');
+aboutItem.innerHTML = '<a id="about__btn" class="navbar__links" href="#about">about</a>';
+
+rightList.appendChild(aboutItem);
+
+const skillsItem = document.createElement('li');
+skillsItem.innerHTML = '<a id="skills__btn" class="navbar__links" href="#skills">skills</a>';
+
+rightList.appendChild(skillsItem);
+
+const projectsItem = document.createElement('li');
+projectsItem.innerHTML = '<a id="projects__btn" class="navbar__links" href="#projects">Projects</a>';
+
+rightList.appendChild(projectsItem);
+
+const contactsItem = document.createElement('li');
+contactsItem.innerHTML = '<a id="contacts__btn" class="navbar__links" href="#contacts">Contacts</a>'
+
+rightList.appendChild(contactsItem);
+
+myDocFrag.appendChild(rightList);
+
+// creating the burger.
+const navBurger = document.createElement('div');
+navBurger.setAttribute('class', 'header__menu');
+navBurger.innerHTML = '<div></div> <div></div> <div></div>'
+
+myDocFrag.appendChild(navBurger);
+
+navbar.appendChild(myDocFrag);
+
+//****************************
+// The global variables.
+//****************************
+
 const navbarLinks = document.getElementsByClassName('navbar__links');
 const burger = document.querySelector('.header__menu');
 const sidebar = document.querySelector('.navbar__right');
+
+
 
 //****************************
 //getting the section position from the top.
@@ -9,6 +63,9 @@ const sidebar = document.querySelector('.navbar__right');
 const getPosition = (element) => {
     return element.getBoundingClientRect().y + window.scrollY;
 }
+
+
+
 
 //********************
 //styling the nav links when its active.
@@ -31,6 +88,9 @@ window.addEventListener('scroll', () => {
     }
 })
 
+
+
+
 //******************
 // handling the navbar links clicks.
 //******************
@@ -42,6 +102,9 @@ for(let i = 0; i < navbarLinks.length; i++) {
         window.scroll({top: getPosition(section), behavior: "smooth"});
     })
 }
+
+
+
 
 //******************
 // handling the burger menu click.
